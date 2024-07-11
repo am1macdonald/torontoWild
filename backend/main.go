39e73ab98a78fdfa3f9a6698bb00ad1f056bc7f5
Adam MacDonald <am1macdonald@gmail.com>
2024-07-11 19:45:56 -0400

    init db in main

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/tursodatabase/go-libsql"
 )
 
 type apiConfig struct {
 }
 
 func main() {
+	dbName := "file:./local.db"
+	db, err := sql.Open("libsql", dbName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s", err)
+		os.Exit(1)
+	}
+
 	mux := http.NewServeMux()
 
 	s := &http.Server{
