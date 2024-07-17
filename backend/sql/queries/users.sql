4ca8ec01a538787e8adc3e404fbbc7e3b6682faf
Adam MacDonald <am1macdonald@gmail.com>
2024-07-17 07:54:45 -0400

    add initial queries

--- backend/sql/queries/users.sql
+++ backend/sql/queries/users.sql
@@ -1,4 +1,17 @@
+-- name: AddUser :exec
+INSERT INTO users
+(email, first_name, last_name)
+VALUES ($1, $2, $3);
+
 -- name: GetUser :one
 SELECT * FROM users
 WHERE email = $1
 LIMIT 1; 
+
+-- name: LoginUser :exec
+UPDATE users SET 
+last_login = now();
+
+-- name: SetUserActive :exec
+UPDATE users SET 
+is_active = true;
