c327ab0da78f9c52a8f4ad00905d6e2d36339be7
Adam MacDonald <am1macdonald@gmail.com>
2024-08-22 07:56:12 -0400

    implement magic link generation

---
+++ backend/internal/magiclink/magiclink.go
@@ -0,0 +1,45 @@
+package magiclink
+
+import (
+	"context"
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/valkey-io/valkey-go"
+)
+
+func generateToken() (string, error) {
+	bytes := make([]byte, 16)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", bytes), nil
+}
+
+func createMagicLink(email string, valkey *valkey.Client, ctx context.Context) (string, error) {
+	token, err := generateToken()
+	if err != nil {
+		return "", err
+	}
+
+	err = (*valkey).Do(ctx, (*valkey).B().Set().Key(token).Value(email).Build()).Error()
+	if err != nil {
+		return "", err
+	}
+
+	err = (*valkey).Do(ctx, (*valkey).B().Expire().Key(token).Seconds(300).Build()).Error()
+	if err != nil {
+		return "", err
+	}
+
+	host := os.Getenv("MAGICLINK_BASE")
+	if host == "" {
+		return "", errors.New("magic link base not set")
+	}
+
+	return fmt.Sprintf(host+"/auth?token%s", token), nil
+}
